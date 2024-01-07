d382f61e6e77fd6798712c784f1be933044cc0aa
Manutencaolondon <152867490+Manutencaolondon@users.noreply.github.com>
2024-01-07 18:46:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+            var inputHoraAtual = document.getElementById("horaAtual");
+            
+            var dataAtual = new Date();
+            var hora = dataAtual.getHours();
+            var minutos = dataAtual.getMinutes();
 
+            // Adiciona um zero à frente se for menor que 10
+            hora = hora < 10 ? "0" + hora : hora;
+            minutos = minutos < 10 ? "0" + minutos : minutos;
+
+            var horaFormatada = hora + ":" + minutos;
+            
+            inputHoraAtual.value = horaFormatada;
+        });
